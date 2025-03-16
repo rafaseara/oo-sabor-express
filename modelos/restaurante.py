d19d3a2396dd3bb1bd7bbe0c25d73fd157d4fb57
rafaseara <rafaseara257@gmail.com>
2025-03-16 16:05:25 -0300

    Colocando um decorador e fazendo ajustes estéticos na listagem do restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,17 +4,24 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False #atributo protegido
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f'{self.nome}   |   {self.categoria}'
     
     def listar_restaurantes():
+        print(f'{'Nome do Restaurante'.ljust(20)}  |  {'Categoria'.ljust(20)}  |  {'Status'.ljust(20)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome}  |  {restaurante.categoria}  |  {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(20)}  |  {restaurante.categoria.ljust(20)}  |  {restaurante.ativo.ljust(20)}')
+
+    @property
+    def ativo(self):
+        return '❎' if self._ativo else '✅'
+
 
 PizzaPrime = Restaurante('PizzaPrime', 'Pizzaria')
 restaurante_praca = Restaurante('Praca', 'Italiana')
 
 Restaurante.listar_restaurantes()
+
